93532c47609684c9c1e827c8d53c2723df1b6019
guruknowledge3-web <guruknowledge3@gmail.com>
2026-04-29 17:40:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,7 @@ generate_mcqs = st.checkbox("Generate MCQs")
 generate_script = st.checkbox("Generate Video Script")
 
  def generate_ai_response(prompt):
-
     response = model.generate_content(prompt)
-
     return response.text
 
 if st.button("Generate"):
